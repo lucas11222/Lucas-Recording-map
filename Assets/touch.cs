447d9b0e3ecc1678e9ddb84855e5a938804a901d
BuildTools <altamiranolucas2013@gmail.com>
2024-09-28 19:47:24 +0200

    add the things

---
+++ Assets/touch.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touch : MonoBehaviour
+{
+        public LayerMask m_LayerMask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MyCollisions();
+    }
+    void MyCollisions()
+    {
+        //Use the OverlapBox to detect if there are any other colliders within this box area.
+        //Use the GameObject's centre, half the size (as a radius) and rotation. This creates an invisible box around your GameObject.
+        Collider[] hitColliders = Physics.OverlapBox(gameObject.transform.position, transform.localScale / 2, Quaternion.identity, m_LayerMask);
+        int i = 0;
+        //Check when there is a new collider coming into contact with the box
+        while (i < hitColliders.Length)
+        {
+            //Output all of the collider names
+            Application.Quit();
+            //Increase the number of Colliders in the array
+            
+        }
+    }
+}
